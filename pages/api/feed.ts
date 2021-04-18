4787b23e91e0a707a432aac8f75bad93c8add08d
Mike Wickett <mike@wickett.ca>
2021-04-18 10:32:57 -0400

    Sort order
    Co-authored-by: Adam McKerlie <adammckerlie@gmail.com>

--- pages/api/feed.ts
+++ pages/api/feed.ts
@@ -5,6 +5,7 @@ import prisma from '../../lib/prisma'
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
   const posts = await prisma.story.findMany({
     where: { approved: true }
+    orderBy: { createdAt: 'asc' }
   })
   res.json(posts)
 }

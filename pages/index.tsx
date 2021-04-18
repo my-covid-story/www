f088ed1e9b8ce5af4f4ae567bf4fe2571b27070a
Mike Wickett <mike@wickett.ca>
2021-04-17 21:32:47 -0400

    render seed data in pre

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,8 @@ import styles from '../styles/Home.module.css'
 
 import StoryForm from '../components/form'
 
-export default function Home() {
+export default function Home({ feed }) {
+  console.log(feed)
   return (
     <div className={styles.container}>
       <Head>
@@ -13,7 +14,22 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>Share your Covid Story</h1>
-        <div className={styles.formContainer}>
+        <div className={styles.storyContainer}>
+          <h3>Latest stories</h3>
+          {feed.map((story) => (
+            <pre>
+              <ul>
+                <li>id: {story.id}</li>
+                <li>content: {story.content}</li>
+                <li>postal: {story.postal}</li>
+                <li>approved: {story.approved}</li>
+                <li>email: {story.email}</li>
+                <li>twitter: {story.twitter}</li>
+              </ul>
+            </pre>
+          ))}
+        </div>
+        <div className={styles.storyContainer}>
           <StoryForm />
         </div>
       </main>
@@ -23,4 +39,12 @@ export default function Home() {
       </footer>
     </div>
   )
+}
+
+export async function getServerSideProps() {
+  const res = await fetch('http://localhost:3000/api/feed')
+  const feed = await res.json()
+  return {
+    props: { feed },
+  }
 }

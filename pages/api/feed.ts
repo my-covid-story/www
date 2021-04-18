d446022acb94270a7621e6f5f9769d709eb2fbf8
Mike Wickett <mike@wickett.ca>
2021-04-18 10:40:41 -0400

    cleanup

--- pages/api/feed.ts
+++ pages/api/feed.ts
@@ -4,7 +4,7 @@ import prisma from '../../lib/prisma'
 
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
   const posts = await prisma.story.findMany({
-    where: { approved: true }
+    where: { approved: true },
     orderBy: { createdAt: 'asc' }
   })
   res.json(posts)

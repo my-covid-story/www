44ccc36500dec3ed9823f022525c3cfcf63fee88
Mike Wickett <mike@wickett.ca>
2021-04-18 15:37:58 -0400

    wip form

--- components/form/index.tsx
+++ components/form/index.tsx
@@ -5,7 +5,7 @@ import {
   FormHelperText,
   Input,
   Button,
-  TextArea,
+  Textarea,
   Checkbox,
   Radio,
   RadioGroup,
@@ -15,62 +15,104 @@ import { useForm } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup'
 
 const schema = yup.object().shape({
-  email: yup.string().email().required(),
-  password: yup.string().min(8).required(),
+  title: yup.string().required(), // TODO: length validation
+  content: yup.string().required(),
+  postal: yup.string().required(), // TODO: need to do postal FSA validation
+  category: yup.string(), // TODO: Figure out enum validation for list options
+  anonymous: yup.bool().required(),
+  contact: yup.bool().required(),
+  name: yup.string(),
+  email: yup.string().email(),
+  phone: yup.string(), // TODO: Phone validation
+  twitter: yup.string(),
 })
 
 type LoginFormInputs = {
-  email: string
-  password: string
+  title: string
+  content: string
+  postal: string
+  category?: string
+  anonymous: boolean
+  contact: boolean
+  name?: string
+  email?: string
+  phone?: string
+  twitter?: string
 }
 
 export default function LoginForm() {
   const {
     register,
     handleSubmit,
+    watch,
     formState: { errors },
   } = useForm<LoginFormInputs>({
     mode: 'onBlur',
     resolver: yupResolver(schema),
   })
 
-  const onSubmit = (values: LoginFormInputs) => console.log(values)
+  const onSubmit = (values: LoginFormInputs) => {
+    console.log('submitted')
+    console.log(values)
+  }
+
+  console.log(watch('postal'))
 
   return (
-    <form style={{ width: 350 }}>
+    <form style={{ width: 350 }} onSubmit={handleSubmit(onSubmit)}>
+      {/* Title field */}
       <FormControl
-        isInvalid={!!errors?.email?.message}
-        errortext={errors?.email?.message}
+        isInvalid={!!errors?.title?.message}
+        errortext={errors?.title?.message}
         p="4"
         isRequired
       >
-        <FormLabel>Email</FormLabel>
-        <Input type="email" name="email" placeholder="Email" {...register('email')} />
-        <FormErrorMessage>{errors?.email?.message}</FormErrorMessage>
-        <FormHelperText>We are obviously giving this straight to Facebook.</FormHelperText>
+        <FormLabel>Enter a title or quote</FormLabel>
+        <Textarea name="title" placeholder="Here is a summary of my story" {...register('title')} />
+        <FormErrorMessage>{errors?.title?.message}</FormErrorMessage>
+        <FormHelperText>Maximum ___ characters</FormHelperText>
       </FormControl>
+
+      {/* Content field */}
       <FormControl
-        isInvalid={!!errors?.password?.message}
-        errortext={errors?.password?.message}
-        px="4"
-        pb="4"
+        isInvalid={!!errors?.content?.message}
+        errortext={errors?.content?.message}
+        p="4"
         isRequired
       >
-        <FormLabel>Password</FormLabel>
-        <Input type="password" placeholder="Password" name="password" {...register('password')} />
-        <FormErrorMessage>{errors?.password?.message}</FormErrorMessage>
+        <FormLabel>Please share your story</FormLabel>
+        <Textarea
+          name="content"
+          placeholder="Here is a summary of my story"
+          {...register('content')}
+        />
+        <FormErrorMessage>{errors?.content?.message}</FormErrorMessage>
+        <FormHelperText>Maximum ___ characters</FormHelperText>
       </FormControl>
+
+      {/* Postal field */}
+      <FormControl
+        isInvalid={!!errors?.postal?.message}
+        errortext={errors?.postal?.message}
+        p="4"
+        isRequired
+      >
+        <FormLabel>First 3 digits of your postal code</FormLabel>
+        <Input type="text" name="postal" {...register('postal')} />
+        <FormErrorMessage>{errors?.postal?.message}</FormErrorMessage>
+      </FormControl>
+
       <Button
-        onClick={handleSubmit(onSubmit)}
+        type="submit"
         p="4"
         mx="4"
         mt="6"
         w="90%"
         colorScheme="blue"
         variant="solid"
-        disabled={!!errors.email || !!errors.password}
+        disabled={!!errors.title || !!errors.content}
       >
-        Login
+        Submit Story
       </Button>
     </form>
   )

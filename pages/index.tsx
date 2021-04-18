8dbacc2e11f634e6c880f9ab0371b90b489eb1c1
Mike Wickett <mike@wickett.ca>
2021-04-17 20:26:57 -0400

    tidy up

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,23 +4,23 @@ import styles from '../styles/Home.module.css'
 import StoryForm from '../components/form'
 
 export default function Home() {
-    return (
-        <div className={styles.container}>
-            <Head>
-                <title>My Covid Story</title>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>My Covid Story</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-            <main className={styles.main}>
-                <h1 className={styles.title}>Share your Covid Story</h1>
-                <div className={styles.formContainer}>
-                    <StoryForm />
-                </div>
-            </main>
-
-            <footer className={styles.footer}>
-                Footer stuff like links, contact info, privacy policy, etc. will go here
-            </footer>
+      <main className={styles.main}>
+        <h1 className={styles.title}>Share your Covid Story</h1>
+        <div className={styles.formContainer}>
+          <StoryForm />
         </div>
-    )
+      </main>
+
+      <footer className={styles.footer}>
+        Footer stuff like links, contact info, privacy policy, etc. will go here
+      </footer>
+    </div>
+  )
 }

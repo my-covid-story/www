6cca8e36d2680961267174237edd32f5ef91fe2e
Mike Wickett <mike@wickett.ca>
2021-04-17 21:34:38 -0400

    comment out global shenanigans

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -10,10 +10,10 @@ let prisma: PrismaClient
 
 if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient()
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient()
-  }
-  prisma = global.prisma
+// } else {
+//   if (!global.prisma) {
+//     global.prisma = new PrismaClient()
+//   }
+//   prisma = global.prisma
 }
 export default prisma

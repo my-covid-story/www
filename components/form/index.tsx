6535d75cac8e4f5045f586a463eef9ced6fa4832
Mike Wickett <mike@wickett.ca>
2021-04-18 21:42:22 -0400

    fix formatting

--- components/form/index.tsx
+++ components/form/index.tsx
@@ -1,26 +1,26 @@
 import s from './form.module.css'
 
 export default function StoryForm() {
-    return (
-        <form className={s.storyForm}>
-            <div className={s.field}>
-                <label className={s.label} htmlFor="story">
-                    Story
-                </label>
-                <textarea name="story" />
-            </div>
-            <div className={s.field}>
-                <label className={s.label} htmlFor="postal">
-                    Postal code
-                    <input type="text" name="postal" />
-                </label>
-            </div>
-            <div className={s.field}>
-                <label className={s.label} htmlFor="photos">
-                    Upload your photo
-                </label>
-                <input type="file" id="photos" name="photos" multiple />
-            </div>
-        </form>
-    )
+  return (
+    <form className={s.storyForm}>
+      <div className={s.field}>
+        <label className={s.label} htmlFor="story">
+          Story
+        </label>
+        <textarea name="story" />
+      </div>
+      <div className={s.field}>
+        <label className={s.label} htmlFor="postal">
+          Postal code
+          <input type="text" name="postal" />
+        </label>
+      </div>
+      <div className={s.field}>
+        <label className={s.label} htmlFor="photos">
+          Upload your photo
+        </label>
+        <input type="file" id="photos" name="photos" multiple />
+      </div>
+    </form>
+  )
 }

9d53994874d934bba5754bcb470769a37282a1ec
Adam McKerlie <adam.mckerlie@sentry.io>
2021-04-19 00:04:35 -0400

    Add Sentry for tracking errors/performance

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,17 @@ import '../styles/globals.css'
 
 import { ChakraProvider } from '@chakra-ui/react'
 
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+import { env } from 'node:process';
+
+Sentry.init({
+  dsn: "https://ff771404287542638b24e14b8de8edff@o573965.ingest.sentry.io/5724646",
+  environment: process.env.NODE_ENV,
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
+
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>

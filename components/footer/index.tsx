0de595af2be92e098a468c0783b8720fbfcbd823
Mike Wickett <mike@wickett.ca>
2021-04-18 21:47:02 -0400

    update formatting

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -3,15 +3,15 @@ import Link from 'next/link'
 import s from './footer.module.css'
 
 export default function Footer() {
-    return (
-      <footer className={s.footer}>
-        <Link href="/">
-          <a>Home</a>
-        </Link>
+  return (
+    <footer className={s.footer}>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
 
-        <Link href="/faq">
-          <a>FAQ</a>
-        </Link>
-      </footer>
-    )
+      <Link href="/faq">
+        <a>FAQ</a>
+      </Link>
+    </footer>
+  )
 }

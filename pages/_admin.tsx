1e16ddf3c2af76f402fae5ccf700a49b479e1b6a
Adam McKerlie <adam.mckerlie@sentry.io>
2021-04-18 22:14:11 -0400

    Add basic auth to Admin endpoints

--- pages/_admin.tsx
+++ pages/_admin.tsx
@@ -5,9 +5,10 @@ import StoryForm from '../components/form'
 
 import { signIn, signOut, useSession } from 'next-auth/client'
 
-export default function _Admin() {
-  const [ session, loading ] = useSession()
-
+export default function _Admin({ feed }) {
+  const [ session ] = useSession()
+  console.log(session);
+  console.log(feed);
   return <>
     {!session && <>
       Not signed in <br/>
@@ -16,6 +17,28 @@ export default function _Admin() {
     {session && <>
       Signed in as {session.user.email} <br/>
       <button onClick={() => signOut()}>Sign out</button>
+
+      {feed && feed.map((story) => (
+        <pre>
+            <ul>
+            <li>id: {story.id}</li>
+            <li>content: {story.content}</li>
+            <li>postal: {story.postal}</li>
+            <li>approved: {story.approved}</li>
+            <li>twitter: {story.twitter}</li>
+            </ul>
+        </pre>
+        ))}
+
     </>}
   </>
+}
+
+
+export async function getServerSideProps({ req }) {
+  const res = await fetch('http://localhost:3000/api/admin', req)
+  const feed = await res.json()
+  return {
+    props: { feed },
+  }
 }

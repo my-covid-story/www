e1f46a633b69c7343664244660729832eb630858
Mike Wickett <mike@wickett.ca>
2021-04-20 17:37:00 -0400

    tweak nesting

--- pages/stories/create.tsx
+++ pages/stories/create.tsx
@@ -5,26 +5,28 @@ import StandardHead from '../../components/standard-head'
 
 export default function CreateStory() {
   return (
-    <div>
+    <>
       <StandardHead
         title="Share your COVID story"
         description="We've all been affected by COVID in someway, share your story and help drive action in Ontario"
       />
-      <Box w="100%" p={8} pb={24} maxWidth="4xl">
-        <Box pb={4}>
-          <Heading as="h1" size="lg" pb={4}>
-            Share your COVID story
-          </Heading>
-          <Text>
-            Every number has a story. We believe in the power of storytelling to force government
-            action in Ontario. We value your privacy.
-          </Text>
-          <Text pt={4} fontWeight="bold" color="primary.100">
-            <Link href="/faq">Read our FAQ</Link>
-          </Text>
+      <div>
+        <Box w="100%" p={8} pb={24} maxWidth="4xl">
+          <Box pb={4}>
+            <Heading as="h1" size="lg" pb={4}>
+              Share your COVID story
+            </Heading>
+            <Text>
+              Every number has a story. We believe in the power of storytelling to force government
+              action in Ontario. We value your privacy.
+            </Text>
+            <Text pt={4} fontWeight="bold" color="primary.100">
+              <Link href="/faq">Read our FAQ</Link>
+            </Text>
+          </Box>
+          <StoryForm />
         </Box>
-        <StoryForm />
-      </Box>
-    </div>
+      </div>
+    </>
   )
 }

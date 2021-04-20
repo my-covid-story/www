214415e8d3461bc06c2c41f58481eae100edf828
Adam McKerlie <adam.mckerlie@sentry.io>
2021-04-20 08:18:59 -0400

    Add ability to Approve and Delete stories

--- pages/_admin.tsx
+++ pages/_admin.tsx
@@ -1,18 +1,25 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 
+import {Button} from '@chakra-ui/react'
+
+import styles from '../styles/Home.module.css'
 import StoryForm from '../components/form'
 
-import { signIn, signOut, useSession, getSession } from 'next-auth/client'
+import { providers, signIn, signOut, useSession, getSession } from 'next-auth/client'
 import prisma from '../lib/prisma'
 
-export default function _Admin({ stories }) {
+export default function _Admin({ stories, providerList }) {
   const [ session ] = useSession()
 
   return <>
     {!session && <>
       Not signed in <br/>
-      <button onClick={() => signIn()}>Sign in</button>
+
+      {Object.values(providerList).map(provider => (
+        <div key={provider.name}>
+          <button onClick={() => signIn(provider.id)}>Sign in with {provider.name}</button>
+        </div>
+      ))}
     </>}
     {session && <>
       Signed in as {session.user.email} <br/>
@@ -21,21 +28,69 @@ export default function _Admin({ stories }) {
       {stories.map((story) => (
         <pre>
             <ul>
-            <li>id: {story.id}</li>
             <li>content: {story.content}</li>
             <li>postal: {story.postal}</li>
             <li>approved: {story.approved}</li>
             <li>twitter: {story.twitter}</li>
             </ul>
+            <Button
+              mt={4}
+              colorScheme="blue"
+              variant="outline"
+              type="button"
+              data-id={story.id}
+              onClick={deleteStory}
+            >
+              Delete
+            </Button>
+            <Button
+              mt={4}
+              colorScheme="blue"
+              type="button"
+              data-id={story.id}
+              onClick={approveStory}
+            >
+              Approve
+            </Button>
         </pre>
         ))}
     </>}
   </>
 }
 
+const deleteStory = async (e) =>  {
+  let story = {
+    id: e.target.dataset.id,
+  };
+
+  let response = await fetch('/api/admin/update', {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8'
+    },
+    body: JSON.stringify(story)
+  });
+}
+
+const approveStory = async (e) =>  {
+  let story = {
+    id: e.target.dataset.id,
+    approved: true
+  };
+
+  let response = await fetch('/api/admin/update', {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8'
+    },
+    body: JSON.stringify(story)
+  });
+}
+
 
 export async function getServerSideProps({ req }) {
   const session = await getSession({ req })
+  const providerList = await providers()
 
   let stories = {}
   if(session) {
@@ -45,7 +100,8 @@ export async function getServerSideProps({ req }) {
     })
   }
 
+
   return {
-    props: { stories },
+    props: { stories, providerList },
   }
 }
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -7,19 +7,19 @@ export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     Providers.Google({
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET
-      })
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
   ],
   adapter: Adapters.Prisma.Adapter({ prisma }),
   // A database is optional, but required to persist accounts in a database
   database: process.env.DATABASE_URL,
   callbacks: {
     async signIn(user, account, profile) {
-      if(process.env.MODERATOR_EMAILS.includes(user.email)){
-          return true
+      if (process.env.MODERATOR_EMAILS.includes(user.email)) {
+        return true
       }
       return false
     },
-  }
+  },
 })

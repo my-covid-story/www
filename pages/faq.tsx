9acdb8bae233746b760b0f5bc6106710ccbeb20e
Jamieson Roberts <hello@jamiesonroberts.com>
2021-04-20 22:19:56 -0400

    Normalizing the word COVID and updating the home page for quick launch

--- pages/faq.tsx
+++ pages/faq.tsx
@@ -12,7 +12,7 @@ export default function FAQ() {
   return (
     <div className={styles.faq}>
       <Head>
-        <title>FAQ - My Covid Story</title>
+        <title>FAQ - My COVID Story</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Box w="95%" p={8} pb={24}>
@@ -47,7 +47,7 @@ export default function FAQ() {
               <AccordionPanel pb={4}>
                 <p>It is clear that our government will not listen to numbers or scientific evidence. Data might be easy to dismiss but stories and the human connection they spark cannot be ignored.</p>
                 <p>Many doctors, business owners, essential workers and concerned citizens have already spoken up and shared their stories, but many are fearful and don’t feel they have a voice; there are too many stories still left to be told.</p>
-                <p>We created MyCovidStory as a way to amplify every voice that wanted to share - with government leaders, the media or the public -  regardless of whether they have their own platform or not.</p>
+                <p>We created My COVID Story as a way to amplify every voice that wanted to share - with government leaders, the media or the public -  regardless of whether they have their own platform or not.</p>
                 <p>We believe the stories speak for themselves, and will use them to engage decision-makers to drive effective government policy and science-based practices that will save lives.</p>
               </AccordionPanel>
             </AccordionItem>

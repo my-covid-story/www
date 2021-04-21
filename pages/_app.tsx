6df1954e32cc8d0b48e67cd3562570361123fe39
Adam McKerlie <adam.mckerlie@sentry.io>
2021-04-21 08:06:01 -0400

    Update to use the Sentry NextJS SDK

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,16 +3,7 @@ import '../styles/globals.css'
 
 import { ChakraProvider } from '@chakra-ui/react'
 
-import * as Sentry from "@sentry/react";
-import { Integrations } from "@sentry/tracing";
 import { env } from 'node:process';
-
-Sentry.init({
-  dsn: "https://ff771404287542638b24e14b8de8edff@o573965.ingest.sentry.io/5724646",
-  environment: process.env.NODE_ENV,
-  integrations: [new Integrations.BrowserTracing()],
-  tracesSampleRate: 1.0,
-});
 
 function MyApp({ Component, pageProps }) {
   return (

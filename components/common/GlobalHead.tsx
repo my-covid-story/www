141ed9c0a75e10ee8603a602f21de87efe831c36
Mike Wickett <mike@wickett.ca>
2021-04-24 17:17:36 -0400

    try with env var

--- components/common/GlobalHead.tsx
+++ components/common/GlobalHead.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head'
 const defaultTitle = 'The Stories of COVID-19 in Ontario | MyCovidStory.ca'
 const defaultDescription =
   "Ontario is in a humanitarian crisis. If our leaders won't listen to the numbers, they must face our stories."
-const defaultPreviewImage = 'https://www.mycovidstory.ca/img/landingpage-v2.jpg'
+const defaultPreviewImage = '/img/landingpage-v2.jpg'
 
 interface GlobalHeadProps {
   title?: string
@@ -16,6 +16,7 @@ export default function GlobalHead({
   description = defaultDescription,
   previewImage = defaultPreviewImage,
 }: GlobalHeadProps): JSX.Element {
+  const imageUrl = `${process.env.NEXT_PUBLIC_VERCEL_URL}${previewImage}`
   return (
     <Head>
       <title key="title">{title}</title>
@@ -23,11 +24,11 @@ export default function GlobalHead({
 
       <meta key="og:title" property="og:title" content={title} />
       <meta key="og:description" property="og:description" content={description} />
-      <meta key="og:image" property="og:image" content={previewImage} />
+      <meta key="og:image" property="og:image" content={imageUrl} />
 
       <meta key="twitter:title" name="twitter:title" content={title} />
       <meta key="twitter:description" name="twitter:description" content={description} />
-      <meta key="twitter:image" name="twitter:image" content={previewImage} />
+      <meta key="twitter:image" name="twitter:image" content={imageUrl} />
     </Head>
   )
 }

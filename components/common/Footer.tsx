703cd521785d841db66c0b38b397690f59e37af0
Jamieson Roberts <hello@jamiesonroberts.com>
2021-04-24 08:53:20 -0400

    Adjusting spacing to make sure the links pass tap target size in lighthouse

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -13,23 +13,43 @@ export default function Footer() {
     >
       <Container centerContent maxW="container.sm">
         <SimpleGrid columns={3} width="100%" textAlign="center">
-          <MenuItem>Home</MenuItem>
-          <MenuItem to="/about">About</MenuItem>
-          <MenuItem to="/faq">FAQ</MenuItem>
-          <MenuItem to="https://kvmhxg5ojy6.typeform.com/to/gUsoYkft" externalLink={true}>
+          <MenuItem padding={1} marginBottom={1}>
+            Home
+          </MenuItem>
+          <MenuItem to="/about" padding={1} marginBottom={1}>
+            About
+          </MenuItem>
+          <MenuItem to="/faq" padding={1} marginBottom={1}>
+            FAQ
+          </MenuItem>
+          <MenuItem
+            to="https://kvmhxg5ojy6.typeform.com/to/gUsoYkft"
+            externalLink={true}
+            padding={1}
+            marginBottom={1}
+          >
             Media Form
           </MenuItem>
-          {/*<MenuItem>Releases</MenuItem>*/}
-          <MenuItem to="https://github.com/my-covid-story/www" externalLink={true}>
+          {/*<MenuItem padding={1} marginBottom={1}>Releases</MenuItem>*/}
+          <MenuItem
+            to="https://github.com/my-covid-story/www"
+            externalLink={true}
+            padding={1}
+            marginBottom={1}
+          >
             Github
           </MenuItem>
           <MenuItem
             to="https://app.usefathom.com/share/xnknpyhv/mycovidstory.ca"
             externalLink={true}
+            padding={1}
+            marginBottom={1}
           >
             Analytics
           </MenuItem>
-          <MenuItem to="mailto:info@mycovidstory.ca">Email Us</MenuItem>
+          <MenuItem to="mailto:info@mycovidstory.ca" padding={1} marginBottom={1}>
+            Email Us
+          </MenuItem>
         </SimpleGrid>
         <Text as="strong" paddingTop={4}>
           Made with love by Ontarians{' '}

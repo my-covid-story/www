75a27217c6dedb9281e6e0bbb784d83cbacf598d
Luca Zoppetti <zoppe2304@gmail.com>
2021-04-25 23:25:26 +0200

    fix: make menu items actual next links instead of anchor tags

--- components/common/MenuItem.tsx
+++ components/common/MenuItem.tsx
@@ -1,11 +1,14 @@
 import { Link, Text } from '@chakra-ui/react'
+import NextLink from 'next/link'
 
 export default function MenuItem({ children, to = '/', externalLink = false, ...rest }) {
   return (
-    <Link href={to} isExternal={externalLink}>
-      <Text display="block" {...rest}>
-        {children}
-      </Text>
-    </Link>
+    <NextLink href={to} passHref>
+      <Link href={to} isExternal={externalLink}>
+        <Text display="block" {...rest}>
+          {children}
+        </Text>
+      </Link>
+    </NextLink>
   )
 }

24fab2d518eb4403810a87ac6e19aaddd790e669
Adam McKerlie <adam.mckerlie@sentry.io>
2021-04-26 09:28:51 -0400

    Strip localization from urls for now

--- next-sitemap.js
+++ next-sitemap.js
@@ -11,4 +11,13 @@ module.exports = {
       },
     ],
   },
+  // TODO: We'll want to remove this once we're localizing content
+  transform: async (config, path) => {
+    return {
+      loc: path.replace('/en-CA', ''),
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+    }
+  },
 }

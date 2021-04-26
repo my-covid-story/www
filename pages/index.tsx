22126664e7b794a0f9f1b99d3f0b9ef92b45a4d8
Adam McKerlie <adammckerlie@gmail.com>
2021-04-26 17:54:56 -0400

    Decrease time to revalidate index page (#115)
    The DB is holding steady. This allows moderated stories to show up 9 minutes faster

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,7 +26,7 @@ export async function getStaticProps() {
   const stories = await list()
   return {
     props: { stories },
-    revalidate: 600, // 10 minutes
+    revalidate: 60, // 1 minutes
   }
 }
 

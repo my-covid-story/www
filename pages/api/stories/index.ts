23516ac0febbd9ca0ecfd829baeaa7f87268cad5
Mike Wickett <mike@wickett.ca>
2021-04-28 08:05:54 -0400

    chore: use baseUrl for story post route

--- pages/api/stories/index.ts
+++ pages/api/stories/index.ts
@@ -2,8 +2,6 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { methodNotAllowed, sendError } from '../../../lib/errors'
 import * as stories from '../../../lib/api/stories'
 
-const baseUrl = 'https://mycovidstory.ca'
-
 // GET, POST /api/stories
 export default function handle(req: NextApiRequest, res: NextApiResponse) {
   switch (req.method) {
@@ -12,7 +10,7 @@ export default function handle(req: NextApiRequest, res: NextApiResponse) {
     case 'POST':
       return stories.add(req.body).then(
         (result) => {
-          res.setHeader('Location', `${baseUrl}${req.url}/${result.id}`)
+          res.setHeader('Location', `${process.env.baseUrl}${req.url}/${result.id}`)
           res.status(201).json(result)
         },
         (err) => sendError(res, err)

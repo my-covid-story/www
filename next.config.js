7bab0d08f2476f732fe79546708eac35846ac5ee
Mike Wickett <mike@wickett.ca>
2021-04-28 08:04:48 -0400

    Fix: correctly set baseUrl

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
-const baseUrl = process.env.VERCEL_ENV
-  ? `https://${process.env.VERCEL_URL}`
-  : `http://localhost:${process.env.PORT || '3000'}`
+let baseUrl
+
+// If we're in production (or this value is set), use this
+if (process.env.NEXT_PUBLIC_BASE_URL) {
+  baseUrl = process.env.NEXT_PUBLIC_BASE_URL
+  // If we're in preview, use the branch URL
+} else if (process.env.VERCEL_ENV === 'preview') {
+  baseUrl = `https://${process.env.VERCEL_URL}`
+  // Fall back to local
+} else {
+  baseUrl = `http://localhost:${process.env.PORT || '3000'}`
+}
 
 console.log(`BASE_URL: ${baseUrl}`)
 

7a0a32bc56ebff73980eea89150e3f0f71aede00
Luca Zoppetti <zoppe2304@gmail.com>
2021-04-29 19:51:56 +0200

    fix: don't trim words in social share

--- lib/social.ts
+++ lib/social.ts
@@ -2,10 +2,20 @@ import { Story } from '@prisma/client'
 import { storyCite } from '../components/stories/model'
 
 export default function generateSocial(story: Story, contentSize = 150, citeSize = 60): string {
-  const storySnippet = story.content.slice(0, contentSize)
-  // Generate a trimmed substring of the story snippet that we're sharing and insert
-  // `...` instead of the last three characters.
-  const trimmedStorySnippet = `${storySnippet.substring(0, storySnippet.length - 3)}...`
+  const { content } = story
+  const cite = storyCite(story).slice(0, citeSize)
 
-  return `"${trimmedStorySnippet}" ${storyCite(story).slice(0, citeSize)}`
+  if (content.length < contentSize) {
+    return `"${content}" ${cite}`
+  } else {
+    const storySnippet = content.slice(0, contentSize)
+
+    // Trim the story to the last space
+    const trimmedStorySnippet = storySnippet.substring(
+      0,
+      Math.min(storySnippet.length, storySnippet.lastIndexOf(' '))
+    )
+
+    return `"${trimmedStorySnippet}..." ${storyCite(story).slice(0, citeSize)}`
+  }
 }

e3dc2df9f2de651a154e9f40c200895dac40bb24
Luca Zoppetti <zoppe2304@gmail.com>
2021-04-29 17:48:20 +0200

    fix: trim social share snippet

--- lib/social.ts
+++ lib/social.ts
@@ -1,6 +1,11 @@
 import { Story } from '@prisma/client'
 import { storyCite } from '../components/stories/model'
 
-export default function generateSocial(story: Story, contentSize = 150, citeSize = 60) {
-  return `"${story.content.slice(0, contentSize)}" ${storyCite(story).slice(0, citeSize)}`
+export default function generateSocial(story: Story, contentSize = 150, citeSize = 60): string {
+  const storySnippet = story.content.slice(0, contentSize)
+  // Generate a trimmed substring of the story snippet that we're sharing and insert
+  // `...` instead of the last three characters.
+  const trimmedStorySnippet = `${storySnippet.substring(0, storySnippet.length - 3)}...`
+
+  return `"${trimmedStorySnippet}" ${storyCite(story).slice(0, citeSize)}`
 }

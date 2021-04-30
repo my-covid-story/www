7e64f2a498b52dc26fe361080c804363977071bc
Luca Zoppetti <luca@luckee.dev>
2021-04-30 13:56:18 +0200

    Fix: trim social share snippet (#149)
    fix: trim social share snippet

--- lib/social.ts
+++ lib/social.ts
@@ -1,6 +1,20 @@
 import { Story } from '@prisma/client'
 import { storyCite } from '../components/stories/model'
 
-export default function generateSocial(story: Story, contentSize = 150, citeSize = 60) {
-  return `"${story.content.slice(0, contentSize)}" ${storyCite(story).slice(0, citeSize)}`
+export default function generateSocial(story: Story, contentSize = 150, citeSize = 60): string {
+  const { content } = story
+  const cite = storyCite(story).slice(0, citeSize)
+
+  if (content.length < contentSize) {
+    return `"${content}" ${cite}`
+  }
+  const storySnippet = content.slice(0, contentSize)
+
+  // Trim the story to the last space
+  const trimmedStorySnippet = storySnippet.substring(
+    0,
+    Math.min(storySnippet.length, storySnippet.lastIndexOf(' '))
+  )
+
+  return `"${trimmedStorySnippet}..." ${storyCite(story).slice(0, citeSize)}`
 }

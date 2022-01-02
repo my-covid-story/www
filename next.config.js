d2aa5ce343ba2988c362a40c5a71d875f72c68d7
Jamieson Roberts <hello@jamiesonroberts.com>
2022-01-02 09:32:20 -0500

    Updating Sentry to the most recent version

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 const { withSentryConfig } = require('@sentry/nextjs')
 const { withSuperjson } = require('next-superjson')
 
-
 let baseUrl
 if (process.env.NEXT_PUBLIC_BASE_URL) {
   baseUrl = process.env.NEXT_PUBLIC_BASE_URL
@@ -23,16 +22,18 @@ const moduleExports = withSuperjson()({
   },
 })
 
-const SentryWebpackPluginOptions = {
+const sentryWebpackPluginOptions = {
   // Additional config options for the Sentry Webpack plugin. Keep in mind that
   // the following options are set automatically, and overriding them is not
   // recommended:
   //   release, url, org, project, authToken, configFile, stripPrefix,
   //   urlPrefix, include, ignore
+
+  silent: true, // Suppresses all logs
   // For all available options, see:
   // https://github.com/getsentry/sentry-webpack-plugin#options.
 }
 
 // Make sure adding Sentry options is the last code to run before exporting, to
 // ensure that your source maps include changes from all other Webpack plugins
-module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions)
+module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions)

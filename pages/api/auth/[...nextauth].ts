7decb1a1c8a95d234a2dff16814c71745fc89a30
Jamieson Roberts <hello@jamiesonroberts.com>
2022-01-02 09:36:27 -0500

    Adding in NextAuth Secret for prod

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -7,6 +7,7 @@ const prisma = new PrismaClient()
 
 export default NextAuth({
   // Configure one or more authentication providers
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
